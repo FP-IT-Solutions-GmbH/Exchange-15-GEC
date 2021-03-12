9ae3da6d7e207f5a82e580f12fca574f11b1bbe9
AJS <aaronschwehm@googlemail.com>
2021-03-12 18:42:37 +0100

    Update var.ps1

--- var.ps1
+++ var.ps1
@@ -1,7 +1,7 @@
-$SAutodiscoverHostname = "autodiscover.voffice-demo.de" #Hostname Autodiscover. e.g. autodiscover.contoso.com
-$SOutlookHostname = "mail.voffice-demo.de" #Hostname Mail. e.g. mail.contoso.com oder outlook.contoso.com
+$SAutodiscoverHostname = "autodiscover.contoso.com" #Hostname Autodiscover. e.g. autodiscover.contoso.com
+$SOutlookHostname = "mail.contoso.com" #Hostname Mail. e.g. mail.contoso.com oder outlook.contoso.com
 $SExchangeProductKey = "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX" #The Exchange Product Key with -
-$SCertFile = "C:\demo-voffice\autodiscover.voffice-demo.de.pfx" #autodiscover cert full path
+$SCertFile = "C:\certfs\cert.pfx" #autodiscover cert full path
 $SCertPassword = "XXXXX" #autodiscover cert password in cleartext
 $SCertFile2 = "H:\exchangeserverauth.pfx"
 $SCertPassword2 = "XXXXX"
